ffbb867935dc3d5957450c8982e1fcbee8a4f6bf
melvoid is king <sameer.knit.ac.in>
2024-01-10 11:21:08 +0530

    sb kuch shi chl rha hai lekin note:- ./constants.js se chl rha hai aur  process.env.port nhi chl rhai over&out

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+import  {DB_NAME}  from "./constants.js";
+
+
+
+
+
+
+
+
+
+
+
+
+/*import express from "express";
+const app = express();
+// const DB_NAME = "videotube";
+;(async()=>{
+
+
+try {
+    
+   await mongoose.connect(`mongodb://localhost:27017/${DB_NAME}`)
+   
+ app.on("error" , ()=>{
+    console.log("we have this error" , error)
+ })
+   app.listen(4500 , ()=>{
+    console.log(`we have connected`);
+   })
+
+} catch (error) {
+    console.error(error);
+}
+})()
+*/
