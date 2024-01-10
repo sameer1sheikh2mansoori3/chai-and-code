34fdc36e411cee4cf15f3207463f868b1d1515b4
melvoid is king <sameer.knit.ac.in>
2024-01-10 20:31:22 +0530

    we are now running process.env.PORT ny changing command look in package.json

--- src/index.js
+++ src/index.js
@@ -2,12 +2,25 @@ import mongoose from "mongoose";
 import  {DB_NAME}  from "./constants.js";
 import dotenv from 'dotenv';
 import myConnect from "./db/index.js";
-dotenv.config({
-   path: './env'
+import { app } from "./app.js";
+// dotenv.config({
+//    path: './env'
+// })
+
+
+myConnect().then(()=>{
+
+app.on("error" , (error)=>{
+   console.log("THIS IS INDEX.JS ERROR: " ,error);
 })
 
+app.listen(process.env.PORT , ()=>{
+   console.log(`we are listing our app ${process.env.PORT}`);
+})
+}).catch((err)=>{
+   console.log(err);
 
-myConnect();
+})
 
 
 
---
+++ src/utils/asynHandler.js
@@ -0,0 +1,19 @@
+export const asynFunction = (fn)=> async( req , res , next )=>{ 
+
+    
+
+    try {
+        
+        await fn( req , res , next )
+
+    } catch (error) {
+
+
+        res.status(error.status || 500).json({
+        success : true,
+        message : `we are having some kind of error ${error.message}`
+        })
+        
+    }
+
+ }
